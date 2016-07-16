109dc9264ebe04ea3b3e67d01476d7a510bbc7f0
Alexis HAMOUDA <ahamouda@e1r2p21.42.fr>
2016-07-17 00:37:23 +0200

    Removed useless variable.

--- Asm/add_file_content_line.c
+++ Asm/add_file_content_line.c
@@ -6,7 +6,7 @@
 /*   By: ahamouda <ahamouda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/16 22:11:48 by ahamouda          #+#    #+#             */
-/*   Updated: 2016/07/17 00:34:34 by ahamouda         ###   ########.fr       */
+/*   Updated: 2016/07/17 00:36:56 by ahamouda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	add_file_content_line(t_data *data, char *line)
 {
 	t_file_line		*new_node;
 	t_file_line		*tmp;
-	int				line_number;
 
 	new_node = ft_memalloc(sizeof(t_file_line));
 	if (!data->file_content)
@@ -28,10 +27,7 @@ void	add_file_content_line(t_data *data, char *line)
 	}
 	tmp = data->file_content;
 	while (tmp && tmp->next)
-	{
 		tmp = tmp->next;
-		line_number = tmp->line_number;
-	}
 	tmp->next = new_node;
 	new_node->line_number = tmp->line_number + 1;
 }
