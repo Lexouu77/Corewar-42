1b0dd8ece0c07dc1b90cf100659d8290e4d5f9b1
Alexis HAMOUDA <ahamouda@e2r11p22.42.fr>
2016-08-22 18:30:31 +0200

    Added add function

--- Vm/add.c
+++ Vm/add.c
@@ -6,7 +6,7 @@
 /*   By: ahamouda <ahamouda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/13 12:19:40 by ahamouda          #+#    #+#             */
-/*   Updated: 2016/08/20 15:38:42 by ahamouda         ###   ########.fr       */
+/*   Updated: 2016/08/22 18:29:34 by ahamouda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 static void	display_add_instruction(t_proc *process, t_vm_data *arena, int i)
 {
+	process->carry = i;
+	if ((arena->verbosity & 8) != 8)
+		return ;
 	ft_printf_fd(arena->fd, "Process number : %d",
 			process->number);
 	ft_printf_fd(arena->fd, " owned by player number : %d",
@@ -22,38 +25,27 @@ static void	display_add_instruction(t_proc *process, t_vm_data *arena, int i)
 	if (i)
 		ft_printf_fd(arena->fd, " And it worked !");
 	else
-	{
 		ft_printf_fd(arena->fd,
 				" And it failed (Reason : invalid register number) !\n");
-		process->carry = 0;
-	}
 }
 
 void		add(t_vm_data *arena, t_proc *process)
-{/*
-	int	tmp_one;
-	int	tmp_two;
-	int	tmp_three;
+{
+	const int	reg = get_param_value(arena, process->pc + 2, 1);
+	const int	reg_two = get_param_value(arena, process->pc + 3, 1);
+	const int	reg_three = get_param_value(arena, process->pc + 4, 1);
 
-	tmp_one = arena->field[(process->pc + 2) % arena->mem_size];
-	tmp_two = arena->field[(process->pc + 3) % arena->mem_size];
-	tmp_three = arena->field[(process->pc + 4) % arena->mem_size];
-	if ((tmp_one >= 0 && tmp_one < REG_NUMBER) && (tmp_two >= 0 &&
-		tmp_two < REG_NUMBER) && (tmp_three >= 0 && tmp_three < REG_NUMBER))
+	if ((check_reg_number(reg - 1) & check_reg_number(reg_two - 1) &
+			check_reg_number(reg_three - 1)) == 0)
 	{
-		if ((arena->verbosity & 8) == 8)
-			display_add_instruction(process, arena, 1);
-		process->reg[tmp_three] = process->reg[tmp_one] + process->reg[tmp_two];
-		process->carry = 1;
-		if ((arena->verbosity & 8) == 8)
-			ft_printf_fd(arena->fd, "It added r%d(%d) and r%d(rd) in r%d\n",
-				tmp_one, process->reg[tmp_one], tmp_two, process->reg[tmp_two],
-				tmp_three);
+		process->pc = (process->pc + 5) % arena->mem_size;
+		return (display_add_instruction(process, arena, 0));
 	}
-	else if ((arena->verbosity & 8) == 8)
-		display_add_instruction(process, arena, 0);
-	else
-		process->carry = 0;
-	process->pc = (process->pc + 5) % arena->mem_size;
-	*/
+	display_add_instruction(process, arena, 1);
+	if ((arena->verbosity & 8) != 8)
+		ft_printf_fd(arena->fd, "It stocked r%d (%d) + r%d (%d) in r%d!\n",
+				reg - 1, process->reg[reg - 1], reg_two,
+				process->reg[reg_two - 1], reg_three - 1);
+	process->reg[reg_three - 1] = process->reg[reg - 1] +
+		process->reg[reg_two - 1];
 }
