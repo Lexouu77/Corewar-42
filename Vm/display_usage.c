58a933fde75371e6c20d81175cf174b9bb296e36
Alexis Hamouda <ahamouda@student.42.fr>
2016-09-13 15:22:31 +0200

    update usage

--- Vm/display_usage.c
+++ Vm/display_usage.c
@@ -6,7 +6,7 @@
 /*   By: ahamouda <ahamouda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/11 22:02:56 by ahamouda          #+#    #+#             */
-/*   Updated: 2016/09/08 17:08:52 by ahamouda         ###   ########.fr       */
+/*   Updated: 2016/09/13 15:22:21 by ahamouda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void			display_usage(void)
 	ft_printf("%[[FG_RED,SP_RST]s [0-9] (Dump) | ", "-d");
 	ft_printf("%[[FG_YEL,SP_RST]s [0-9] (Set Player Number) | ", "-n");
 	ft_printf("%[[FG_BLU,SP_RST]s [0-9] (Graphic Mod [Shell-only]) | ", "-l");
-	ft_printf("%[[FG_GRE,SP_RST]s [1|2|4|8] (Verbosity Mod) | ", "-v");
+	ft_printf("%[[FG_GRE,SP_RST]s [1|2|4|8|16] (Verbosity Mod) | ", "-v");
 	ft_printf("%[[FG_CYA,SP_RST]s (Graphic Mod) | ", "-g");
 	ft_printf("%[[FG_CYA,SP_RST]s (Create a Log File)]", "-f");
 	ft_printf(" [file%[[FG_MAG,SP_RST]s] [file%[[FG_MAG,SP_RST]s] ...\n",
@@ -34,9 +34,8 @@ void			display_usage(void)
 	ft_printf("-Playing with no valid players won't trigger the arena.\n");
 	ft_printf("-Using someone who isn't tagged with the COREWAR_EXEC_MAGIC");
 	ft_printf(" won't validate his participation.\n");
-	ft_printf("-Dump and Loop Dump bonuses can't work together.\n");
+	ft_printf("-Dump and Graphic mod (Shell) bonuses can't work together.\n");
 	ft_printf("-Graphic will automatically set Color + Log File and ");
-	ft_printf("unset Loop Dump + Dump.\n");
-	ft_printf("-Press [SPACE] to pause the vm in Graphic mode. -/+ to set the");
-	ft_printf(" time between each cycles.\n");
+	ft_printf("unset Graphic (Shell) + Dump.\n");
+	ft_printf("-Press [SPACE] to pause the vm in Graphic mode.\n");
 }
